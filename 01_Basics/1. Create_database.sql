7770a0ba587d46409453c0d45da9f9d8cbf4e181
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-03-10 00:02:50 +0530

    Update 1. Create_database.sql

--- 01_Basics/1. Create_database.sql
+++ 01_Basics/1. Create_database.sql
@@ -5,3 +5,9 @@ CREATE DATABASE temp2;
 -- Delete a database
 DROP DATABASE temp1;
 DROP DATABASE temp2;
+
+--Create a Student Database
+CREATE DATABASE student;
+
+-- Select the database to create tables
+USE student;
