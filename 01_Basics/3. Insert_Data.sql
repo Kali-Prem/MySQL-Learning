1ba8187354f17c743a756937c1f54e0c8cd2f873
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-03-10 00:36:00 +0530

    Create 3. Insert_Data.sql

---
+++ 01_Basics/3. Insert_Data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Student (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+);
+
+-- Data Insert in student table
+ 
+INSERT INTO student VALUES (1, "Kali Linux", 20);
+INSERT INTO student VALUES (2, "HACKER", 20);
+INSERT INTO student VALUES (3, "ParrotOS" , 30);
